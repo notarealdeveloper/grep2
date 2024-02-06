ed8dc6d838678a97c73d7cd8b3d277c80cec2be8
Jason Wilkes <notarealdeveloper@gmail.com>
2024-02-05 17:02:05 -0800

    Correct address tests

--- tests/test_address.py
+++ tests/test_address.py
@@ -7,10 +7,10 @@ addresses = {
     'files/address/03.txt': "800 Ocean View Blvd, Pacifica, CA 94044",
     'files/address/04.txt': "24 Maple Avenue,\nRiverdale, NY, 10471",
     'files/address/05.txt': "6789 College St, Boston, MA 02115",
-    'files/address/06.txt': "The Grand Hotel\n789 Broadway Blvd\nNew York, NY 10003",
+    'files/address/06.txt': "789 Broadway Blvd\nNew York, NY 10003",
     'files/address/07.txt': "321 Cedar Ln, Knoxville, TN 37912",
     'files/address/08.txt': "5678 Park Place, Seattle, WA 98101",
-    'files/address/09.txt': "ACME Corporation\n9876 Elm Street\nChicago, IL 60607",
+    'files/address/09.txt': "9876 Elm Street\nChicago, IL 60607",
 }
 
 def test_addresses():
